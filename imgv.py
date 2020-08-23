65744957abcc2f5201085912965e0aae9890a7a6
Jan Ivanecky <jan.ivanecky@hotmail.com>
2020-08-22 23:35:40 -0700

    Make GET parameters absolute paths

--- imgv.py
+++ imgv.py
@@ -4,42 +4,38 @@ import os
 
 app = Flask(__name__)
 
-# Initialize to home dir.
-current_dir = os.path.abspath("")
-
 # Serve at most 100 images to keep things sane.
 MAX_IMG_LIMIT = 100
 
-@app.route('/imgs/<filename>')
-def get_img(filename):
-    return send_from_directory(current_dir, filename)
+@app.route('/imgs/<path:img>')
+def get_img(img):
+    # The GET parameter doesn't contain the initial slash, so we have to re-add it.
+    img = '/' + img
+    return send_from_directory(os.path.dirname(img), os.path.basename(img))
 
-@app.route('/', defaults={'next_dir': None})
-@app.route('/<next_dir>')
+@app.route('/', defaults={'next_dir': os.path.expanduser('~')})
+@app.route('/<path:next_dir>')
 def main(next_dir):
-    # Good'ol global state.
-    global current_dir
+    # The GET parameter doesn't contain the initial slash, so we have to re-add it.
+    current_dir = '/' + next_dir
 
-    if next_dir:
-        # `^` is an URL-representable symbol for going up the directory, so we need to parse it.
-        if next_dir == '^':
-            next_dir = '..'
-
-        current_dir = os.path.join(current_dir, next_dir)
-    else:
-        # Reinitialize to home dir.
-        current_dir = os.path.abspath("")
-
-    # 
+    #
     files = os.listdir(current_dir)
     
     # Create a list of sub-directories.
     dirs = [f for f in files if os.path.isdir(os.path.join(current_dir, f))]
     dirs = sorted(dirs)
-    dirs = ["^"] + dirs  # Add "go up" option.
+    # Make into tuples of (dir_name, dir_path).
+    # Note that we're stripping first slash from the path so it can be used as a GET parameter.
+    dirs = [(d, os.path.join(current_dir, d).lstrip('/')) for d in dirs]
+    # Add "go up" option.
+    dirs = [("..", os.path.dirname(current_dir).lstrip('/'))] + dirs
 
     # Create a list of images to display.
     imgs = [f for f in files if any([f.endswith(ext) for ext in ['png', 'jpg', 'jpeg']])]
+    # Make into tuples of (img_name, img_path).
+    # Note that we're stripping first slash from the path so it can be used as a GET parameter.
+    imgs = [(i, os.path.join(current_dir, i).lstrip('/')) for i in imgs]
     imgs = imgs[:MAX_IMG_LIMIT]
 
     # Serve.
